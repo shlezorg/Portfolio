7440b564dff0621542029b3ced9222d4f1e3a189
shlezorg <shlezorg@gmail.com>
2026-05-23 00:53:15 +0530

    Contact backend

--- script.js
+++ script.js
@@ -5,3 +5,29 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 }
+
+emailjs.init("nE3wh-uaJoubwfXEV");
+
+document
+.getElementById("contact-form")
+.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    emailjs.sendForm(
+        "service_9wh7a3p",
+        "template_fbaep8l",
+        this
+    )
+
+    .then(() => {
+        alert("Message Sent Successfully!");
+        this.reset();
+    })
+
+    .catch((error) => {
+        console.log(error);
+        alert("Sending Failed!");
+    });
+
+});
